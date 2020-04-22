636667a65668a834c5a83d4b222a484ca558cb42
Pedro Elias <epedro520@gmail.com>
2020-04-22 15:26:26 -0300

    New CRUD app in Node js framework created

---
+++ private/create-table.js
@@ -0,0 +1,8 @@
+const mysql =  require('mysql')
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port:'3306',
+    user:'root',
+    password:'1234',
+    database:'NodeGit'
+})
