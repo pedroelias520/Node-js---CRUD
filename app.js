fc68a7195aa8b8855c25c0530396c31d5092cdd0
Pedro Elias <epedro520@gmail.com>
2020-10-30 12:22:04 -0300

    upgrade: project refactor

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app =express();
 const router = require('./router')
 const port = 3030
 
-
+app.use('public',express.static('public'));
 app.set('views','private/src/views/pages');
 app.set('view engine','ejs');
-app.use(express.static('public'));
 app.use('/',router);
+app.use(express.static(__dirname +'/public'))
 app.listen(port,()=>console.log("Server Running -> 3030"))
